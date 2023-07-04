87193d47a2c7dd3737b72a4bc25ffef3d3cd1500
Matan Dessaur <matandessaur@m8ndoes.com>
2023-07-04 11:02:18 -0400

    Added Localized Loading

--- src/App.js
+++ src/App.js
@@ -710,7 +710,7 @@ export default function App() {
           )}
         </SelectedRestaurant>
         <ButtonWrapper>
-          <LocalisedButton onClick={toggleLocalized} localized={localized}>
+          <LocalisedButton onClick={toggleLocalized} localized={localized} disabled={!randomRestaurant}>
             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 2C7.59 2 4 5.589 4 9.995 3.971 16.44 11.696 21.784 12 22c0 0 8.03-5.56 8-12 0-4.411-3.589-8-8-8Zm0 12c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4Z"></path>
             </svg>
