db35e8a8e10d20bcdf420feba2146cbaff370375
Matan Dessaur (M8N) <126172236+M8N-MatanDessaur@users.noreply.github.com>
2023-07-20 22:40:51 +0000

    Added ContextMenu for IOS

--- src/Components/LocalizationButton.jsx
+++ src/Components/LocalizationButton.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from "styled-components";
 
 export default function LocalizationButton({ toggleLocalized, currentLocation, localized, randomRestaurant, toggleRad, radius }) {
     const [touchStart, setTouchStart] = useState(0);
