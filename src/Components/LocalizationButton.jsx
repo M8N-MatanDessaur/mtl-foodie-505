82ecac5eac9fb585ac9f5f122b7f413a42b79efa
Matan Dessaur (M8N) <126172236+M8N-MatanDessaur@users.noreply.github.com>
2023-07-20 22:27:45 +0000

    Added ContextMenu for IOS

--- src/Components/LocalizationButton.jsx
+++ src/Components/LocalizationButton.jsx
@@ -1,15 +1,38 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
 
 export default function LocalizationButton({ toggleLocalized, currentLocation, localized, randomRestaurant, toggleRad, radius }) {
+    const [touchStart, setTouchStart] = useState(0);
+    const [touchEnd, setTouchEnd] = useState(0);
+
+    const handleTouchStart = (e) => {
+        setTouchStart(new Date().getTime());
+    }
+
+    const handleTouchEnd = (e) => {
+        setTouchEnd(new Date().getTime());
+
+        // If touch duration was more than 500 milliseconds, treat it as a long press
+        if ((touchEnd - touchStart) > 500) {
+            toggleRad(e);
+        }
+    }
+
     return (
-        <Button radius={radius}  onContextMenu={e=>toggleRad(e)} onClick={toggleLocalized} currentLocation={currentLocation} localized={localized} disabled={currentLocation === null}>
+        <Button 
+            radius={radius}  
+            onTouchStart={handleTouchStart} 
+            onTouchEnd={handleTouchEnd} 
+            onClick={toggleLocalized} 
+            currentLocation={currentLocation} 
+            localized={localized} 
+            disabled={currentLocation === null}>
             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 2C7.59 2 4 5.589 4 9.995 3.971 16.44 11.696 21.784 12 22c0 0 8.03-5.56 8-12 0-4.411-3.589-8-8-8Zm0 12c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4Z"></path>
             </svg>
         </Button>
     )
 }
+
 
 const Button = styled.button`
   height: 60px;
