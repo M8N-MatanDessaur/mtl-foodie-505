fed31cadd7fe5a7c71af0aa169b91cdab59d4559
Matan Dessaur <matandessaur@m8ndoes.com>
2023-07-24 13:25:37 -0400

    small fixes

--- functions/fetch-description.js
+++ functions/fetch-description.js
@@ -6,7 +6,7 @@ exports.handler = async function(event, context) {
 
     const response = await axios.post("https://api.openai.com/v1/engines/text-davinci-002/completions", 
       {
-        prompt: `Créez une blague drole et amusante qui se passe dans un restaurant nommé "${restaurantName}" en quebecois sans trop décrire le restaurant ou les plats. En max 1024 charactères`,
+        prompt: `Écrit une blague drole et amusante qui se passe dans un restaurant nommé "${restaurantName}".`,
         max_tokens: 1000,
       },
       {
