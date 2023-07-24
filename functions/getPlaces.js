2489b89e43c060dd3cca523a2bd7e80fb05057cf
Matan Dessaur <matandessaur@m8ndoes.com>
2023-07-23 22:12:50 -0400

    Added more types to API

--- functions/getPlaces.js
+++ functions/getPlaces.js
@@ -2,8 +2,10 @@ const axios = require('axios');
 
 exports.handler = async function(event, context) {
     const { latitude, longitude, radius, type, key } = event.queryStringParameters;
+    const types = "restaurant|cafe|bar|bakery|bistro|buffet|diner|food_court|pizza|ice_cream|fast_food|fine_dining|breakfast|brunch|lunch|dinner|pub|taco|steakhouse|sushi|bbq|vegetarian|food_truck|indian|chinese|italian|mexican|greek|thai|seafood|burger|dessert|ramen|french|spanish|mediterranean|korean|vietnamese|american|german|middle_eastern|caribbean|peruvian|brazilian|ethiopian|vegan|gluten_free"; 
 
-    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&type=${type}&key=${key}`;
+
+    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&type=${types}&key=${key}`;
 
     try {
         const { data } = await axios.get(url);
