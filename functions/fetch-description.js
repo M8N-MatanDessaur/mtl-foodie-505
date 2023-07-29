f54b9ffa59555577ad9d5286c020e525b0dfb775
Matan Dessaur <matandessaur@m8ndoes.com>
2023-07-29 08:37:20 -0400

    get language fix

--- functions/fetch-description.js
+++ functions/fetch-description.js
@@ -4,9 +4,13 @@ exports.handler = async function(event, context) {
   try {
     const restaurantName = event.queryStringParameters.name;
 
+    const preferredLanguage = getPreferredLanguageFromHeader(event.headers['Accept-Language']);
+
+    const prompt = `Imagine a funny joke or pun that takes place in a restaurant named "${restaurantName}" in ${preferredLanguage}.`;
+
     const response = await axios.post("https://api.openai.com/v1/engines/text-davinci-003/completions", 
       {
-        prompt: `Imaginez une blague ou un jeu de mots drôle et amusant(e) qui se passe dans un restaurant appelé "${restaurantName}".`,
+        prompt,
         temperature: 0.7,
         max_tokens: 1000,
       },
@@ -23,12 +27,12 @@ exports.handler = async function(event, context) {
     if (data && data.choices && data.choices.length > 0) {
       return {
         statusCode: 200,
-        body: JSON.stringify({ description: " "+data.choices[0].text.trim() }),
+        body: JSON.stringify({ description: data.choices[0].text.trim() }),
       };
     } else {
       return {
         statusCode: 200,
-        body: JSON.stringify({ description: `Asteur écoute icitte, j'ai des spots de bouffe qui vont t'en faire glousser dans ton p'tit bedon! T'as l'estomac qui crie pour une poutine à te faire baver dans ton hoodie? Pas d'soucis, mon chum! Y'a des places pour ça, j'te dis! Pis si t'es plutôt d'humeur pour du smoked meat tendre à te faire fondre l'coeur, y'a des endroits pour ça aussi, crissement! Et pour ceux qui aiment les fruits de mer, y'a un coin qui va te faire décoller le palais, osti! J'te dis pas où, mais ça vaut la peine d'explorer! Ah, pis pour les amateurs de burgers, y'a un spot qui va te faire saliver comme un loup affamé! J'te laisse découvrir par toi-même, mon pote! Faque là, mon chum, prends ton hoodie, ton sens de l'humour et vas-y découvrir ces spots où tu risques de baver et de rire en même temps!` }),
+        body: JSON.stringify({ description: `I'm sorry, I couldn't generate a joke for the restaurant "${restaurantName}".` }),
       };
     }
   } catch (error) {
@@ -38,3 +42,7 @@ exports.handler = async function(event, context) {
     };
   }
 };
+
+function getPreferredLanguageFromHeader(acceptLanguageHeader) {
+  return acceptLanguageHeader.split(',')[0];
+}
